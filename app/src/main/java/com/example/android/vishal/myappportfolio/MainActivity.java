6a9eddd406dca3981d5f6070edcfb3df8461e405
Vishal <visgiri@cisco.com>
2016-07-09 18:48:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/vishal/myappportfolio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.vishal.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Context;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
